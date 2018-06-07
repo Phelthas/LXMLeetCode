df5446beebd43cdf1a60fac081b6048ab7fdb83b
Phelthas <billthas@gmail.com>
2018-06-07 11:31:50 +0800

    合并了两个有序数组

--- LXMLeetCode/Sort/SortSolution.swift
+++ LXMLeetCode/Sort/SortSolution.swift
@@ -180,3 +180,35 @@ extension SortSolution {
     
 }
 
+
+extension SortSolution {
+    
+
+    /// 合并了两个有序数组
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
+        while i >= 0 || j >= 0 {
+            if i < 0 {
+                nums1[k] = nums2[j]
+                j = j - 1
+                k = k - 1
+            } else if j < 0 {
+                nums1[k] = nums1[i]
+                i = i - 1
+                k = k - 1
+            } else if nums1[i] >= nums2[j] {
+                nums1[k] = nums1[i]
+                i = i - 1
+                k = k - 1
+            } else {
+                nums1[k] = nums2[j]
+                j = j - 1
+                k = k - 1
+            }
+        }
+    }
+}
+
