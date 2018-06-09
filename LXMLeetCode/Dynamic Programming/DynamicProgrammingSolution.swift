abdf0547f8b62956747e1bd845d65f1c52a0dc92
Phelthas <billthas@gmail.com>
2018-06-09 14:26:33 +0800

    动态规划，爬楼梯问题

---
+++ LXMLeetCode/Dynamic Programming/DynamicProgrammingSolution.swift
@@ -0,0 +1,39 @@
+//
+//  DynamicProgrammingSolution.swift
+//  LXMLeetCode
+//
+//  Created by luxiaoming on 2018/6/9.
+//  Copyright © 2018年 duowan. All rights reserved.
+//
+
+import Foundation
+
+
+class DynamicProgrammingSolution {
+    
+    // 爬楼梯，递归算法，会超时，因为有很多重复计算
+    // func climbStairs(_ n: Int) -> Int {
+    //     if n == 1 {
+    //         return 1
+    //     }
+    //     if n == 2 {
+    //         return 2
+    //     }
+    //     return climbStairs(n - 1) + climbStairs(n - 2)
+    // }
+    
+//    爬楼梯，用数组保存的方法
+    func climbStairs(_ n: Int) -> Int {
+        if n <= 2 {
+            return n
+        }
+        var array: [Int] = [1, 2]
+        for i in 2 ... n - 1 {
+            let temp = array[i - 1] + array[i - 2]
+            array.append(temp)
+        }
+        return array[n - 1]
+    }
+    
+    
+}
