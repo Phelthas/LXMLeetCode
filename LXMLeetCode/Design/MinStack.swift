63986bbdb10d97b5c85c08345de5341890709950
Phelthas <billthas@gmail.com>
2018-06-14 11:23:13 +0800

    题目384. 打乱数组，倒着取的算法比较好理解

--- LXMLeetCode/Design/MinStack.swift
+++ LXMLeetCode/Design/MinStack.swift
@@ -39,3 +39,31 @@ class MinStack {
     }
 }
 
+
+
+/// 384. 打乱数组
+class ShuffleAnArray {
+    
+    private var nums: [Int]
+    
+    init(nums: [Int]) {
+        self.nums = nums
+    }
+    
+    func reset() -> [Int] {
+        return self.nums
+    }
+    
+    func shuffle() -> [Int] {
+        
+        var temp = nums
+        for i in (0 ..< nums.count).reversed() {
+            //arc4random_uniform()产生0-n之间的随机数，包括0和n-1,不包括n
+            let j = arc4random_uniform(UInt32(i + 1))
+            temp.swapAt(i, Int(j))
+        }
+        return temp
+    }
+    
+}
+
