3d07dfa47b2d5652dc9cbabae7da7b032602d6f4
Phelthas <billthas@gmail.com>
2018-06-19 10:33:02 +0800

    题目461，汉明距离

--- LXMLeetCode/Other/Other.swift
+++ LXMLeetCode/Other/Other.swift
@@ -23,4 +23,22 @@ class OtherSolution {
         }
         return result;
     }
+    
+    
+    /// 题目461，汉明距离
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        var m = x
+        var n = y
+        var result = 0
+        while n != 0 || m != 0 {
+            let a = m % 2
+            let b = n % 2
+            if a != b {
+                result += 1
+            }
+            m = m >> 1
+            n = n >> 1
+        }
+        return result
+    }
 }
