5b89ed5edbe70d3956a9db1925abf5f5a06219a6
luxiaoming <billthas@gmail.com>
2018-08-16 21:54:53 +0800

    /// 167. 两数之和 II - 输入有序数组

--- LXMLeetCode/String/StringSolution.swift
+++ LXMLeetCode/String/StringSolution.swift
@@ -329,4 +329,21 @@ extension StringSolution {
         }
         return result
     }
+    
+    /// 167. 两数之和 II - 输入有序数组
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var i = 0
+        var j = numbers.count - 1
+        while i < j {
+            let sum = numbers[i] + numbers[j]
+            if sum == target {
+                return [i + 1, j + 1]
+            } else if sum < target {
+                i += 1
+            } else {
+                j -= 1
+            }
+        }
+        return [-1, -1]
+    }
 }
