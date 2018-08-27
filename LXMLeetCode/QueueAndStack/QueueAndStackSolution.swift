a6a0c15e4609450373318738f8e54866e9f14ac9
Phelthas <billthas@gmail.com>
2018-08-27 15:56:08 +0800

    修改了一下字符串强转，居然过了

--- LXMLeetCode/QueueAndStack/QueueAndStackSolution.swift
+++ LXMLeetCode/QueueAndStack/QueueAndStackSolution.swift
@@ -14,24 +14,45 @@ class QueueAndStackSolution {
     func openLock(_ deadends: [String], _ target: String) -> Int {
         var result = 0
         var queue = ["0000"]
-        var set = Set<String>()
-        set.insert("0000")
+        var set = Set<String>(deadends)
+        if set.contains("0000") { return -1 }
+        
+        func plusCharacter(_ c: Character) -> Character {
+            switch c {
+            case "0": return "1"
+            case "1": return "2"
+            case "2": return "3"
+            case "3": return "4"
+            case "4": return "5"
+            case "5": return "6"
+            case "6": return "7"
+            case "7": return "8"
+            case "8": return "9"
+            default: return "0"
+            }
+        }
+        func minusCharacter(_ c: Character) -> Character {
+            switch c {
+            case "1": return "0"
+            case "2": return "1"
+            case "3": return "2"
+            case "4": return "3"
+            case "5": return "4"
+            case "6": return "5"
+            case "7": return "6"
+            case "8": return "7"
+            case "9": return "8"
+            default: return "9"
+            }
+        }
         
         func helper(_ s: String) {
-            if deadends.contains(s) {
-                return
-            }
             set.insert(s)
-            var array = [Character](s)
+            let array = [Character](s)
             for i in 0 ..< 4 {
                 var temp = array
-                let value = Int(String(array[i]))!
-                let one = (value + 1) % 10
-                var two = value - 1
-                if two == -1 {
-                    two = 9
-                }
-                let c1 = Character("\(one)")
+                let c = temp[i]
+                let c1 = plusCharacter(c)
                 temp[i] = c1
                 let s1 = String(temp)
                 if !set.contains(s1) {
@@ -39,7 +60,7 @@ class QueueAndStackSolution {
                     queue.append(s1)
                 }
                 
-                let c2 = Character("\(two)")
+                let c2 = minusCharacter(c)
                 temp[i] = c2
                 let s2 = String(temp)
                 if !set.contains(s2) {
