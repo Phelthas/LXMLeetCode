4a2be7b85cc20e4c829f83da9cb708756feca020
luxiaoming <billthas@gmail.com>
2018-09-18 23:47:25 +0800

    /// 40. 组合总和 II 完全抄的，没看懂

--- LXMLeetCode/Library/LibrarySolution.swift
+++ LXMLeetCode/Library/LibrarySolution.swift
@@ -230,4 +230,31 @@ class LibrarySolution {
         helper(array: array, start: 0, temp: [Int](), sum: 0)
         return result
     }
+    
+    /// 40. 组合总和 II 完全抄的，没看懂
+    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var sortNums = candidates.sorted()
+        var tmp = [Int]()  // current set for testing
+        var rlt = [[Int]]() // the result set
+        
+        // subtrack a num from candidates to find the combination:
+        func backtrack(_ nums: inout [Int], _ tmp: inout [Int], _ start:Int, _ tg:Int, _ rlt:inout[[Int]]) {
+            if tg==0 { // find one and put into result;
+                rlt.append(tmp)
+                return
+            }
+            if start >= nums.count || nums[start] > tg { return } // go on condictions
+            for i in start...nums.count-1 {
+                if tg < nums[i] { return }
+                if i != start && nums[i]==nums[i-1] { continue } // skip the same num been used
+                tmp.append(nums[i])
+                backtrack(&nums, &tmp, i+1, tg-nums[i], &rlt)
+                tmp.removeLast()
+            }
+        }
+        
+        backtrack(&sortNums, &tmp, 0, target, &rlt)
+        return rlt
+    }
+    
 }
