86288f9c0bf17d2d21fe246110eed8fae508f9d0
luxiaoming <billthas@gmail.com>
2018-09-26 23:25:30 +0800

    /// 64. 最小路径和

--- LXMLeetCode/Library/LibrarySolution.swift
+++ LXMLeetCode/Library/LibrarySolution.swift
@@ -445,5 +445,26 @@ class LibrarySolution {
         }
         return s
     }
-
+    
+    /// 64. 最小路径和
+    func minPathSum(_ grid: [[Int]]) -> Int {
+        let m = grid.count
+        if m == 0 { return 0 }
+        let n = grid[0].count
+        if n == 0 { return 0 }
+        var matrix = grid
+        for i in 1 ..< m {
+            matrix[i][0] = matrix[i - 1][0] + matrix[i][0]
+        }
+        for j in 1 ..< n {
+            matrix[0][j] = matrix[0][j - 1] + matrix[0][j]
+        }
+        
+        for i in 1 ..< m {
+            for j in 1 ..< n {
+                matrix[i][j] = min(matrix[i - 1][j], matrix[i][j - 1]) + matrix[i][j]
+            }
+        }
+        return matrix[m - 1][n - 1]
+    }
 }
