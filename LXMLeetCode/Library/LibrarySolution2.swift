2906442a8753de6c85dbd13fad7f0857cbbf1ef7
luxiaoming <billthas@gmail.com>
2018-10-20 23:32:07 +0800

    RNG居然输给了G2....代码都不想敲了

---
+++ LXMLeetCode/Library/LibrarySolution2.swift
@@ -0,0 +1,13 @@
+//
+//  LibrarySolution2.swift
+//  LXMLeetCode
+//
+//  Created by kook on 2018/10/20.
+//  Copyright © 2018年 duowan. All rights reserved.
+//
+
+import Foundation
+
+extension LibrarySolution {
+    
+}
