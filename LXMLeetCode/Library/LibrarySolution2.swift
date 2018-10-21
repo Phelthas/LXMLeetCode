7ac36ac7b792061817837fac76088dde5d4d1bd7
luxiaoming <billthas@gmail.com>
2018-10-21 22:52:15 +0800

    /// 107. 二叉树的层次遍历 II

--- LXMLeetCode/Library/LibrarySolution2.swift
+++ LXMLeetCode/Library/LibrarySolution2.swift
@@ -10,4 +10,28 @@ import Foundation
 
 extension LibrarySolution {
     
+    /// 107. 二叉树的层次遍历 II
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        var result = [[Int]]()
+        guard let root = root else { return result }
+        var arrayOne = [TreeNode]()
+        arrayOne.append(root)
+        var arrayTwo = [TreeNode]()
+        while arrayOne.count > 0 {
+            var temp = [Int]()
+            for node in arrayOne {
+                temp.append(node.val)
+                if let left = node.left {
+                    arrayTwo.append(left)
+                }
+                if let right = node.right {
+                    arrayTwo.append(right)
+                }
+            }
+            result.append(temp)
+            arrayOne = arrayTwo
+            arrayTwo.removeAll()
+        }
+        return result.reversed()
+    }
 }
