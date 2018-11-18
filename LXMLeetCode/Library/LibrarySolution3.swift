aa3247cba161ff6c3a0d31221bbd791f08eafe9d
luxiaoming <billthas@gmail.com>
2018-11-18 23:29:22 +0800

    /// 226. 翻转二叉树

--- LXMLeetCode/Library/LibrarySolution3.swift
+++ LXMLeetCode/Library/LibrarySolution3.swift
@@ -277,4 +277,15 @@ extension LibrarySolution {
             return area1 + area2
         }
     }
+    
+    /// 226. 翻转二叉树
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else { return nil }
+        let temp = root.left
+        root.left = root.right
+        root.right = temp
+        invertTree(root.left)
+        invertTree(root.right)
+        return root
+    }
 }
