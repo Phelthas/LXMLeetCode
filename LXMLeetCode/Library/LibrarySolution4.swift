7e9f5bd51438acf90e017dfe27b6431e75075541
luxiaoming <billthas@gmail.com>
2018-12-29 23:27:16 +0800

    /// 400. 第N个数字，这个居然只是个 简单？

--- LXMLeetCode/Library/LibrarySolution4.swift
+++ LXMLeetCode/Library/LibrarySolution4.swift
@@ -725,6 +725,66 @@ extension LibrarySolution {
         return result
     }
     
+    /// 400. 第N个数字,一开始想的算法，超时了
+    /*
+     public  static int findNthDigit(int n) {
+     int digit = 1;//digit level, we start at one digit.
+     long counts = 9;//the number counts of current digit level,we start at one digit,there are 9 numbers(1-9)which is at one digit.
+     /**
+     * number [1-9] (there are 9 numbers)is of one digit,number[10-99](there are 90 numbers) is
+     * of two digits,number[100-999](there are 900 numbers) is of three digits,so first we should
+     * find what level(i mean which digits(one digit,two digit or so on)  by level) the nth digit locate,
+     * once we find the digit level, we achieve half the process,
+     */
+     /**
+     *if n - digit * counts > 0,it means the nth digit is not at the current digit level,we should
+     * increase digit level to pass more number
+     */
+     
+     while (n - digit * counts > 0) {
+     //every time we pass the number at current digit level
+     n -= digit * counts;
+     
+     digit++;
+     //counts are grow as follows,9,90,900,9000.....since the counts maybe overflow so i use long type
+     counts *= 10;
+     }
+     //after loop,the n means nth digits from the current baseNumber
+     
+     //the base number is 1，10，100，1000，10000 and so on.
+     int baseNumber = (int)Math.pow(10, digit - 1);
+     //find the number where nth digit locate
+     int number  = (n -1) / digit + baseNumber;
+     //find the digit where nth digit locate at the number above
+     int mod = (n - 1 ) % digit;
+     return String.valueOf(number).charAt(mod) - '0';
+     }
+     
+     */
+//    func findNthDigit(_ n: Int) -> Int {
+//        if  n == 0 { return 0 }
+//        var str = ""
+//        var i = 1
+//        while str.count < n {
+//            str += "\(i)"
+//            i += 1
+//        }
+//        return Int(String([Character](str)[n - 1]))!
+//    }
+    func findNthDigit(_ n: Int) -> Int {
+        var digit = 1
+        var counts = 9
+        var n = n
+        while n - digit * counts > 0 {
+            n -= digit * counts
+            digit += 1
+            counts *= 10
+        }
+        let baseNumber = Int(pow(Float(10), Float(digit - 1)))
+        let num = (n - 1) / digit + baseNumber
+        let mod = (n - 1) % digit
+        return Int(String([Character]("\(num)")[mod]))!
+    }
     
 
     /// 404. 左叶子之和
